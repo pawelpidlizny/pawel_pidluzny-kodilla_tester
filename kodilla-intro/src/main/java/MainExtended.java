bd848b80e03a1791f59b4d4a4320009356224e73
Pawel Pidluzny <pawelpidluzny@MacBook-Pro-Pawel.local>
2020-10-15 14:09:55 +0300

    New Task

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String example = "Hello to Kodillia!";
+        String myName = "Pawel";
+        int myAge = 36;
+        char firstLetter = 'P';
+        double myHeight = 1.90;
+        boolean isFather = true;
+
+        System.out.println(example);
+        System.out.println("Is Father: " + isFather );
+        System.out.println("Name: " + myName);
+        System.out.println("Age: " + myAge);
+        System.out.println("First Letter: " + firstLetter);
+        System.out.println("My Height: " + myHeight);
+
+    }
+}
