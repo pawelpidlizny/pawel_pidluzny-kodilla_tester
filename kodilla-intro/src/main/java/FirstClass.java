76bc58b99ce9a25e9f1b67eedc311ab88740dba5
pawelpidlizny <pawelpidluzny@gmail.com>
2020-10-23 21:53:08 +0300

    Loops

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,8 +1,19 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String[] movies = new String[] {"Harry Potter", "Lord of the Rings", "Forrest Gump"};
+        String[] movies = new String[5];
+
+        movies[0] = "Harry Potter";
+        movies[1] = "Lord of the Rings";
+        movies[2] = "Forrest Gump";
+        movies[3] = "Trylogy";
+        movies[4] = "Hobbit";
 
         int number0fElements = movies.length;
         System.out.println("Movies length: "+ movies.length);
+
+
+        for (int i = 0; i <=4; i ++ ) {
+            System.out.println("Array element movies[" + i + "] = " + movies[i]);
+        }
     }
 }
--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Loops {
+public class Loops {
+    public static void main(String[] args){
+       int[] numbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = i + 1;
+            System.out.println(i);
+
+        }
+    }
 }
