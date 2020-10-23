74ce77659b85c0c6561956be4010b8c2a856c773
Pawel Pidluzny <pawelpidluzny@MacBook-Pro-Pawel.local>
2020-10-23 20:38:00 +0300

    3 linijki

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,11 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+        String[] movies = new String[] {"Harry Potter", "Lord of the Rings", "Forrest Gump"};
 
-        if (1 > 2) {
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
+        int number0fElements = movies.length;
+        System.out.println("Movies length: "+ movies.length);
     }
 }
