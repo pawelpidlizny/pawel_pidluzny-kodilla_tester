625dff2764f105cf7e08b9899e8e4ba52fe2ab55
pawelpidlizny <pawelpidluzny@gmail.com>
2020-11-19 22:01:48 +0200

    User

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,19 +1,16 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String[] movies = new String[5];
 
-        movies[0] = "Harry Potter";
-        movies[1] = "Lord of the Rings";
-        movies[2] = "Forrest Gump";
-        movies[3] = "Trylogy";
-        movies[4] = "Hobbit";
+        Notebook notebook = new Notebook("600g",1000);
+        System.out.println (notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
 
-        int number0fElements = movies.length;
-        System.out.println("Movies length: "+ movies.length);
+        Notebook heavyNotebook = new Notebook("2000g",1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
 
-
-        for (int i = 0; i <=4; i ++ ) {
-            System.out.println("Array element movies[" + i + "] = " + movies[i]);
-        }
+        Notebook oldNotebook = new Notebook ("1200g",500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook .price);
+        oldNotebook.checkPrice();
     }
 }
