525983f31aaf37dbaa3fca412684478b0760fd8d
pawelpidlizny <pawelpidluzny@gmail.com>
2020-11-22 20:11:48 +0200

    2.3 Notebook

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,16 +1,42 @@
 public class Notebook {
     String weight;
     int price;
+    int year;
 
-    public Notebook (String weight,int price){
+    public Notebook (String weight,int price,int year){
         this.weight = weight;
         this.price = price;
+        this.year = year;
     }
     public void checkPrice(){
-        if (this.price<900) {
-            System.out.println("This notebook is cheap.");
-        } else {
-            System.out.println("This notebook is quite expensive.");
+        if (this.price<600){
+            System.out.println("This notebook is cheap, Ten notebook jest tani.");
+        } else if (this.price<=1000) {
+            System.out.println("The price is good, Cena jest good.");
+        } else if (this.price>1000){
+            System.out.println("This notebook is  expensive, Ten notebook jest drogi.");
+        }
+    }
+    public void checkWeight(){
+       if (this.weight == "600g"){
+           System.out.println("This notebook is light,Ten notebook jest lekki.");
+       } else if (this.weight=="1600g"){
+           System.out.println("This notebook is heavy, Ten notebook jest ciężki");
+       } else if (this.weight=="2000"){
+           System.out.println("This notebook is not very heavy,Ten notebook jest bardzo ciężki");
+       }
+    }
+    public void checkYearAndPrice(){
+        if (this.year < 2015 && this.price >= 600 && this.price <= 1000) {
+            System.out.println("This computer is old and at moderate price");
+        } else if (this.year > 2015 && this.price >= 600 && this.price <= 1000) {
+            System.out.println("This computer is new and at moderate price");
+        } else if (this.year < 2015 && this.price < 600) {
+            System.out.println("This computer is old and cheap");
+        } else if (this.year > 2015 && this.price < 600) {
+            System.out.println("This computer is new and cheap");
+        }if (this.year > 2015 && this.price > 1000){
+            System.out.println("This computer is new and expensive");
         }
     }
 }
