0a1dfd50363e3b115077877de2b6367105059caa
pawelpidlizny <pawelpidluzny@gmail.com>
2020-11-25 21:13:48 +0200

    Zmienne i metody statystyczne

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,20 @@
+public class Book {
+    private String author;
+    private String title;
+    public  static Book of (String author,String title){
+        return new Book(author, title);
+
+    }
+    public Book (String author,String title){
+
+        this.author = author;
+        this.title = title;
+    }
+
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+        System.out.println(book.title + " "  + book.author );
+
+    }
+}
