1593ad7f493f2be29241ed99c65b060c53e0aa07
pawelpidlizny <pawelpidluzny@gmail.com>
2020-12-11 21:31:01 +0200

    Zadanie: Testy jednostkowe dla kalkulatora

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -2,8 +2,11 @@ package com.kodilla.basic_assertion;
 
 
 import com.kodilla.abstracts.basic_assertion.Calculator;
+import com.kodilla.abstracts.basic_assertion.ResultChecker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.awt.geom.Area;
 
 
 public class CalculatorTestSuite {
@@ -16,7 +19,41 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         Assertions.assertEquals(13, sumResult);
 
+        boolean correct = ResultChecker.assertEquals(13,sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + " " + a + "" + "i" + " " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb" + " " + " " + a + " " + "i" + " " + b);
+        }
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int x = 5;
+        int y = 8;
+        int subtractResult = calculator.subtract(x,y);
+        Assertions.assertEquals(-3, subtractResult);
+
+        boolean subtract =ResultChecker.assertEquals(13, subtractResult );
+        if (subtract) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + x + " i " + y);
+
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + x + " i " + y);
+        }
+        int z = 8;
+        int powerResult = calculator.power(z);
+        Assertions.assertEquals(64,powerResult);
+        boolean correctPower = ResultChecker.assertEquals(64,powerResult);
+
+        if (correctPower){
+            System.out.println("Metoda subtract działa poprawnia dlia liczb " + z );
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + z);
+        }
 
     }
-}
 
+
+}
