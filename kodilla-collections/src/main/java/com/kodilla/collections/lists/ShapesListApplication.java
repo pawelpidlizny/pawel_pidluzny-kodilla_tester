4073306c0c0bdeb32dda2fe6255266226e73a5a8
pawelpidlizny <pawelpidluzny@gmail.com>
2021-01-13 18:27:48 +0200

    4.3. Kolekcje – listy Czym są kolekcje?

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/ShapesListApplication.java
@@ -0,0 +1,25 @@
+package com.kodilla.collections.lists;
+import com.kodilla.collections.interfaces.Square;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapesListApplication {
+
+    public static void main(String[] args) {
+        List <Square> shapes = new ArrayList<>();
+
+        Square sq1 = new Square(10.0);
+        Square sq2 = new Square(5.0);
+        Square sq3 = new Square(1.0);
+
+        shapes.add(sq1);
+        shapes.add(sq2);
+        shapes.add(sq3);
+
+
+        for (Square square : shapes) {
+            square.getArea();
+            System.out.println(square + ",area:" + square.getArea());
+        }
+    }
+}
