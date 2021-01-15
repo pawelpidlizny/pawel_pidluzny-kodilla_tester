82d5e7a68d2d178018c6d13e3418e6075b0d266a
pawelpidlizny <pawelpidluzny@gmail.com>
2021-01-15 19:30:04 +0200

    Zadanie: szkoły i dyrektorzy

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/homework/School.java
@@ -0,0 +1,30 @@
+package com.kodilla.collections.adv.maps.complex.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+
+    private String schoolName;
+
+    private final List<Integer> numberOfStudents = new ArrayList<>();
+
+    public School(String schoolName, int... numberOfStudents) {
+        this.schoolName = schoolName;
+        for (int student : numberOfStudents)
+            this.numberOfStudents.add(student);
+
+    }
+
+    public int sumOfStudents() {
+        int sum = 0;
+        for (Integer numberOfStudent : numberOfStudents) {
+            sum += numberOfStudent;
+        }
+        return sum;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+}
