7bcbbdcb7773acedc15fe8a0ea5cb8942e43b380
pawelpidlizny <pawelpidluzny@gmail.com>
2021-01-18 23:54:26 +0200

    Zadanie: księgozbiór fix

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -3,20 +3,13 @@ package com.kodilla.collections.adv.immutable.special.homework;
 import java.util.HashSet;
 
 public class BookManager {
-    public static HashSet <Book> books = new HashSet<>();
+    public static HashSet<Book> books = new HashSet<>();
 
     public static Book createBook (String title, String author) {
-
-        for (Book book : books) {
-            if (book.getAuthor().equals(author) && book.getTitle().equals(title)) {
-                return book;
-            }
-        }
 
         Book newBook = new Book(title,author);
         books.add(newBook);
         return newBook;
-
 
     }
 }
