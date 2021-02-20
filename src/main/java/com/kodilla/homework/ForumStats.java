ec65836bbc38d8939ab05a7bd0b8cffa72b66efa
pawelpidlizny <pawelpidluzny@gmail.com>
2021-02-20 22:29:36 +0200

    6.3. Strumienie typów liczbowych fix

--- src/main/java/com/kodilla/homework/ForumStats.java
+++ src/main/java/com/kodilla/homework/ForumStats.java
@@ -5,23 +5,30 @@ import com.kodilla.stream.UsersRepository;
 
 public class ForumStats {
     public static void main(String[] args) {
-        UsersRepository.getUserList()
+        ForumStats forumStats = new ForumStats();
+
+        forumStats.average();
+        forumStats.average2();
+    }
+    public void average() {
+        double avg = UsersRepository.getUserList()
                 .stream()
-                .filter(user -> user.getAge()>=40)
+                .filter(user -> user.getAge() >= 40)
                 .map(ForumStats::NumberOfPosts)
                 .mapToInt(n -> n)
-                .getClass();
-        System.out.println("Średnia postów użytkowników w wieku 40 lat i więcej wynosi : " );
-
-        UsersRepository.getUserList()
+                .average()
+                .orElseThrow();
+        System.out.println("Średnia postów użytkowników w wieku 40 lat i więcej wynosi : " + avg);
+    }
+    public void average2(){
+       double avg2 =  UsersRepository.getUserList()
                 .stream()
                 .filter(u -> u.getAge() < 40)
-                .map(ForumStats:: NumberOfPosts)
+                .map(ForumStats::NumberOfPosts)
                 .mapToInt(n -> n)
                 .average()
-                .getAsDouble();
-        System.out.println("Średnia postów użytkowników w wieku poniżej 40: " );
-
+               .orElseThrow();
+       System.out.println("Średnia postów użytkowników w wieku poniżej 40: " + avg2);
     }
     public static int NumberOfPosts(User user) {
         return user.getNumberOfPost();
