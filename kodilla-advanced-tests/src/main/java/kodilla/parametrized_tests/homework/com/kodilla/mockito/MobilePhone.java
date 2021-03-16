bc3d9de8b29bf7ad0171d7012e8f7ab2333aaca2
pawelpidlizny <pawelpidluzny@gmail.com>
2021-03-16 21:31:53 +0200

    7.5. Mockito – wstęp

---
+++ kodilla-advanced-tests/src/main/java/kodilla/parametrized_tests/homework/com/kodilla/mockito/MobilePhone.java
@@ -0,0 +1,8 @@
+package kodilla.parametrized_tests.homework.com.kodilla.mockito;
+
+public interface MobilePhone {
+
+    boolean needsCharging();
+    double getFreeStorage();
+    void launchApplication(String applicationName);
+}
