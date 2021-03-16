b338767927059c295cfeb8d91594af828f76a765
pawelpidlizny <pawelpidluzny@gmail.com>
2021-03-16 13:27:00 +0200

    7.3. Obiekty jako dane wejściowe

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/ShouldReverseStringWithLowerCaseTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/ShouldReverseStringWithLowerCaseTest.java
@@ -4,8 +4,10 @@ import kodilla.parametrized_tests.StringValidator;
 import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.stream.Stream;
 
 
 class ShouldReverseStringWithLowerCaseTest {
@@ -14,6 +16,16 @@ class ShouldReverseStringWithLowerCaseTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
     public void shouldReverseStringWithLowerCase(String input, String expected) {
-       Assertions.assertEquals (expected, manipulator.reverseWithLowerCase(input));
+        Assertions.assertEquals(expected, manipulator.reverseWithLowerCase(input));
+    }
+
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
     }
 }
